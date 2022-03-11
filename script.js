a4d3491331c676078f31fe2999e1e35ba4857a99
Wendy <ng_yih_wei@hotmail.com>
2022-03-11 14:22:10 -0800

    Add calculator function and update operator functions

--- script.js
+++ script.js
@@ -4,21 +4,39 @@ let currentDisplayNumber = document.querySelector('[data-current-number]');
 let previousDisplayNumber = document.querySelector('[data-previous-number]');
 let allClearButton = document.querySelector('[data-all-clear]');
 let clearButton = document.querySelector('[data-clear]');
+let equalButton = document.querySelector('[data-equal]');
 let currentNumber = '';
 let previousNumber = '';
+let operator = '';
 
 numbers.forEach(button => {
     button.addEventListener('click', () => {
       currentDisplayNumber.innerHTML = currentDisplayNumber.innerHTML + button.innerHTML;
-    
+      currentNumber = parseInt(currentDisplayNumber.innerText);
     })
 })
 
 operators.forEach(button => {
   button.addEventListener('click', () =>{
-    if(isNaN(previousDisplayNumber.innerHTML.slice(-1)) && currentDisplayNumber.innerHTML === '') return
-    previousDisplayNumber.innerHTML = `${previousDisplayNumber.innerHTML} ${currentDisplayNumber.innerHTML} ${button.innerHTML}`;
-    currentDisplayNumber.innerHTML = '';
+    if(isNaN(previousDisplayNumber.innerHTML.slice(-1)) 
+    && currentDisplayNumber.innerHTML === '') return
+    if(operator === ''){
+      previousDisplayNumber.innerHTML = `${currentDisplayNumber.innerHTML} ${button.innerHTML}`;
+      currentDisplayNumber.innerHTML = '';
+      calculator();
+      previousNumber = currentNumber;
+      currentNumber = '';
+      operator = button.innerHTML;
+      
+    }else if (operator !== ''){
+      calculator();
+      previousDisplayNumber.innerHTML = `${currentNumber} ${button.innerHTML}`;
+      currentDisplayNumber.innerHTML = '';
+      previousNumber = currentNumber;
+      currentNumber = '';
+      operator = button.innerHTML;
+    }
+    
   })
 })
 
@@ -33,10 +51,39 @@ function clear(){
   currentDisplayNumber.innerText = currentDisplayNumber.innerText.slice(0, -1);
 }
 
+//calculator function
+function calculator(){
+  if(previousNumber === '' && currentNumber === '') return
+  switch(operator){
+    case '÷':
+      currentNumber = previousNumber / currentNumber;
+      break;
+    case '+':
+      currentNumber = previousNumber + currentNumber;
+      break;
+    case '-':
+      currentNumber = previousNumber - currentNumber;
+      break;
+    case 'x':
+      currentNumber = previousNumber * currentNumber;
+      break;
+    default:
+      return
+  }
+  previousNumber = '';
+  operator = '';
+}
+
 allClearButton.addEventListener('click', () => {
   clearAll();
 })
 
 clearButton.addEventListener('click', () => {
   clear();
+})
+
+equalButton.addEventListener('click', () => {
+  calculator();
+  currentDisplayNumber.innerText = currentNumber;
+  previousDisplayNumber.innerText = '';
 })
